379f66abcee23f97d46a3031ad76fcd3e881b332
zeeshanhanif <zeeshanhanif@gmail.com>
2016-09-11 13:48:10 +0500

    Udacity Course ud853 Lesson 4 Part 4 -- Initial Repo

---
+++ Sunshine6/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_setting){
+            Intent settingActivityIntent = new Intent(this,SettingActivity.class);
+            startActivity(settingActivityIntent);
+            return true;
+        }
+        else if(item.getItemId() == R.id.action_map){
+            openPreferredLocationInMap();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPref.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q",location).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if(intent.resolveActivity(getPackageManager()) !=null){
+            startActivity(intent);
+        }
+        else {
+            Log.e(TAG,"Couldn't call "+location+", on Map");
+        }
+    }
+}
